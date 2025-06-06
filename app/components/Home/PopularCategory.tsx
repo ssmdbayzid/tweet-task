461b7d44e82386f32a3d01f171b3f2da0dcde4a2
ssmdbayzid <ssmdbayzid@gmail.com>
2025-06-06 18:47:09 +0600

    update

--- app/components/Home/PopularCategory.tsx
+++ app/components/Home/PopularCategory.tsx
@@ -10,16 +10,21 @@ import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { GoArrowLeft, GoArrowRight } from 'react-icons/go';
+import type { Swiper as SwiperCore } from 'swiper'; 
 
 const PopularCategory = () => {
-  const prevRef = useRef(null);
-  const nextRef = useRef(null);
-  const [navigationReady, setNavigationReady] = useState(false);
-
+  const prevRef = useRef<HTMLButtonElement>(null); 
+  const nextRef = useRef<HTMLButtonElement>(null); 
+  const [swiperInstance, setSwiperInstance] = useState<SwiperCore | null>(null); 
   useEffect(() => {
-    // Wait for refs to be attached
-    setNavigationReady(true);
-  }, []);
+    if (swiperInstance && prevRef.current && nextRef.current) {      
+      if (swiperInstance.params.navigation) { 
+        (swiperInstance.params.navigation as any).prevEl = prevRef.current; 
+        (swiperInstance.params.navigation as any).nextEl = nextRef.current; 
+        swiperInstance.navigation.update(); 
+      }
+    }
+  }, [swiperInstance, prevRef, nextRef]); 
 
   return (
     <div className='section container'>
@@ -31,44 +36,40 @@ const PopularCategory = () => {
       </div>
 
       <div className="relative ">
-        {/* Swiper */}
-        {navigationReady && (
-          <Swiper
-            modules={[Navigation]}
-            spaceBetween={20}
-            slidesPerView={1}
-            navigation={{
-              prevEl: prevRef.current,
-              nextEl: nextRef.current,
-            }}
-            onBeforeInit={(swiper) => {
-              swiper.params.navigation.prevEl = prevRef.current;
-              swiper.params.navigation.nextEl = nextRef.current;
-            }}
-            breakpoints={{
-              1024: {
-                slidesPerView: 7,
-                spaceBetween: 24
-              }
-            }}
-          >
-            {categoryData && categoryData.map((category, index) => (
-              <SwiperSlide key={index} className=''>
-                <div
-                  style={{ backgroundColor: `${category?.bgColor}` }}
-                  className={`relative h-[160px] min-w-[150px] rounded-md`}
-                >
-                  <div className="absolute h-[128px] w-[95%] mx-auto bottom-8">
-                    <div className='!min-h-[130px] max-h-[120px]'>
-                      <Image src={category?.img} fill className='!object-contain' alt='category image' />
-                    </div>
-                    <p className='text-center text-white'>{category?.name}</p>
+      
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={20}
+          slidesPerView={1}
+          navigation={{ 
+            prevEl: prevRef.current,
+            nextEl: nextRef.current,
+          }}
+          onSwiper={setSwiperInstance} 
+          breakpoints={{
+            1024: {
+              slidesPerView: 7,
+              spaceBetween: 24
+            }
+          }}
+        >
+          {categoryData && categoryData.map((category, index) => (
+            <SwiperSlide key={index} className=''>
+              <div
+                style={{ backgroundColor: `${category?.bgColor}` }}
+                className={`relative h-[160px] min-w-[150px] rounded-md`}
+              >
+                <div className="absolute h-[128px] w-[95%] mx-auto bottom-8">
+                  <div className='!min-h-[130px] max-h-[120px]'>
+                    <Image src={category?.img} fill className='!object-contain' alt='category image' />
                   </div>
+                  <p className='text-center text-white'>{category?.name}</p>
                 </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        )}
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
 
         {/* Custom Buttons */}
         <div className="absolute top-1/2 -left-20 z-10 -translate-y-1/2">
@@ -78,7 +79,7 @@ const PopularCategory = () => {
         </div>
         <div className="absolute top-1/2 -right-20 z-10 -translate-y-1/2">
           <button ref={nextRef} className="swiperNavButton">
-         <GoArrowRight size={25} />
+           <GoArrowRight size={25} />
           </button>
         </div>
 
@@ -88,6 +89,3 @@ const PopularCategory = () => {
 };
 
 export default PopularCategory;
-
-
-
--- app/components/Home/Testimonial.tsx
+++ app/components/Home/Testimonial.tsx
@@ -5,7 +5,7 @@ import TestimonialCard from '../TestimonialCard'
 
 const Testimonial = () => {
   return (
-    <div className='section'>
+    <div className='section mb-28'>
       <div className=" py-20 flex justify-start items-center gap-30">  
   <div className="flex w-1/2 flex-col items-start text-left">
     <div className="mb-[50px]">
@@ -24,9 +24,9 @@ const Testimonial = () => {
     <img src="https://i.ibb.co/xqGmMHbs/plate.png" alt="Salad Bowl" className="z-20 w-full rotate-3 rounded-full object-cover" />
     <img src="https://i.ibb.co/1tmv9ryf/chilli.png" alt="Green Chili" className="absolute top-2 left-10 z-10 " />
     <img src="https://i.ibb.co/GvQQKFN5/vagitable.png" alt="Peas" className="absolute top-10 -right-1/3 z-30 w-1/2 rotate-0" />
-    <img src="https://i.ibb.co/DfPMSvzs/potato.png" alt="Tomato" className="absolute -bottom-1/2 left-1/3 w-1/2 rotate-180 object-cover" />
+    <img src="https://i.ibb.co/DfPMSvzs/potato.png" alt="Tomato" className="absolute -bottom-[40%] left-1/3 w-1/2 rotate-180 object-contain" />
     
-    <div className="absolute h-[350px] w-[350px] rounded-full border-2 border-dashed border-primary/50"></div>
+    <div className="absolute h-[380px] w-[380px] rounded-full border-2 border-dashed border-primary/50"></div>
     <div className="absolute h-[400px] w-[400px] scale-130 rounded-full border-2 border-dashed border-primary/50"></div>
   </div>
 </div>
